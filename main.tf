20b1ade9f8276191265a7a56032925c6b4670671
Rich Wandell <richwandell@gmail.com>
2020-02-09 18:08:22 -0500

    first

--- main.tf
+++ main.tf
@@ -42,5 +42,53 @@ resource "aws_lambda_function" "lambda" {
   }
 }
 
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "ServerlessExample"
+  description = "Terraform Serverless Application Example"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  parent_id = aws_api_gateway_rest_api.example.root_resource_id
+  path_part = "{proxy+}"
+  rest_api_id = aws_api_gateway_rest_api.example.id
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  authorization = "NONE"
+  http_method = "ANY"
+  resource_id = aws_api_gateway_resource.proxy.id
+  rest_api_id = aws_api_gateway_rest_api.example.id
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  http_method = aws_api_gateway_method.proxy.http_method
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.lambda.invoke_arn
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [
+    aws_api_gateway_integration.lambda
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name = "test"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.example.invoke_url
+}
 
 
