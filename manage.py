854e717e39d8b35b63666258b0ec0f29e30e344d
Rich Wandell <richwandell@gmail.com>
2020-02-23 20:07:16 -0500

    hi

--- manage.py
+++
@@ -1,8 +0,0 @@
-from flask_migrate import Migrate
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from .app import create_app
-
-app = create_app(Flask)
-db = SQLAlchemy()
-migrate = Migrate(app, db)
